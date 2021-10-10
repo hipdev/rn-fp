6d57a8b9292567a3140393861374bc114a15ac8e
Julián David <julianfullstackdev@gmail.com>
2021-10-10 09:24:41 -0500

    flex values on elements inside a flex parent

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,24 +4,34 @@ import { View, Text, StyleSheet } from 'react-native'
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child #1</Text>
-      <Text style={styles.textStyle}>Child #2</Text>
-      <Text style={styles.textStyle}>Child #3</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   viewStyle: {
-    flexDirection: 'row',
     borderWidth: 3,
     borderColor: 'black',
     height: 200,
     justifyContent: 'space-around',
   },
-  textStyle: {
+  textOneStyle: {
     borderWidth: 3,
     borderColor: 'red',
+    flex: 4,
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    flex: 4,
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    flex: 2,
   },
 })
 
